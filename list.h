870378c58a52650b9f84b606a9e1258f88515c70
Lixiao66 <913131032@qq.com>
2019-04-26 11:53:58 +0800

    添加注释

--- list.h
+++ list.h
@@ -1,12 +1,19 @@
+/*********************
+    File Name:list.h
+    Author:李肖
+    Mail:mark.li@fanvil.com
+    Description: 定义链表节点的结构体以及对链表操作函数接口的声明
+*********************/
 #ifndef LIST_H
 #define LIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/*定义链表节点的结构体*/
 typedef struct node{
-	int data;
-	struct node *next;
+    int data;  /*节点中存放的数据*/
+	struct node *next;  /*下一节点*/
 }Linklist;
 
 extern Linklist* List_create();
@@ -17,6 +24,6 @@ extern void List_traverse(Linklist *L);
 extern int Node_delete(Linklist *L,int num);
 extern int Node_found(Linklist *L,int num);
 extern int Node_set(Linklist *L,int num,int data);
-extern int Linklist_delete(Linklist *L);
+extern void Linklist_delete(Linklist *L);
 
 #endif
